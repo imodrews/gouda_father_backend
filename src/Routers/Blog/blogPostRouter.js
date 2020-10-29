d140ca851194ab099c701b484049b19c31bd3368
Alejandra Ossa Jimenez <alejandra.ossa.jimenez@gmail.com>
2020-10-29 16:42:51 +0100

    deleted mockup data

--- src/Routers/Blog/blogPostRouter.js
+++ src/Routers/Blog/blogPostRouter.js
@@ -1,51 +1,57 @@
 const express = require("express");
+const { query } = require("express");
+const client = require("../../client");
 const blogPostRouter = express.Router();
 
-const blogPost = [
-  { 
-    id: 1,
-    title: "Cheese Wars: the Return of the Cheddar",
-    slug: "cheeseWars",
-    heroImage: "https://images.ctfassets.net/d88iaf7z86dj/6F4EJTcQtzDJ3A3jMPoLp0/41e017032776a01a18323220e98f046f/valentin-ancelin-heisenberg-2.jpg.png",
-    blogEntryImage: "https://images.ctfassets.net/d88iaf7z86dj/6F4EJTcQtzDJ3A3jMPoLp0/41e017032776a01a18323220e98f046f/valentin-ancelin-heisenberg-2.jpg.png",
-    author: "Alejandra",
-    publishDate: 2020-10-04,
-    blogShortdDescription: "test",
-  }, 
-  { 
-    id: 1,
-    title: "Blue Cheese and the Cuckoo's Nest",
-    slug: "blueCheese",
-    heroImage: "https://images.ctfassets.net/d88iaf7z86dj/6F4EJTcQtzDJ3A3jMPoLp0/41e017032776a01a18323220e98f046f/valentin-ancelin-heisenberg-2.jpg.png",
-    blogEntryImage: "test",
-    author: "Imogen",
-    publishDate: 2020-10-20,
-    blogShortdDescription: "test",
-  },
-  { 
-    id: 1,
-    title: "Requiem for a Gruyere",
-    slug: "requiemGruyere",
-    heroImage: "https://images.ctfassets.net/d88iaf7z86dj/6F4EJTcQtzDJ3A3jMPoLp0/41e017032776a01a18323220e98f046f/valentin-ancelin-heisenberg-2.jpg.png",
-    blogEntryImage: "test",
-    author: "Martin",
-    publishDate: 2020-10-15,
-    blogShortdDescription: "tests",
-  } 
 
-]
 blogPostRouter.get("/", (req, res, next) => {
-	res.send(blogPost);
+  client 
+    .query("SELECT * FROM blog")
+    .then((data) => res.json(data.rows))
+    .catch((err) => console.log(err))
 });
 
 blogPostRouter.get("/:slug", (req, res, next) => {
-  const currentBlog = blogPost.find(
-    (blog) => blog.slug === req.params.slug);
-    if (currentBlog) {
-      res.status(200).send(currentBlog);
-  } else {res.status(400).send("MIEEEEEEEEEEP: WRONG!!!")}
-	
+  const { slug } = req.params;
+  client
+    .query("SELECT * FROM blog WHERE slug=$1", [slug])
+    .then((data) => res.json(data.rows))
+    .catch((err) => console.log(err))
 });
 
+blogPostRouter.post("/:slug", (req, res, next) => {
+  const {
+    title, 
+    slug, 
+    heroImageUrl, 
+    blogEntryImage, 
+    author,
+    publishdate, 
+    blogShortDescription, 
+    blogContentRich
+  } = req.body;
+  const text = "INSERT INTO blog (title, slug, heroImageUrl, blogEntryImage, author, publishDate, blogShortDescription,blogContentRich VALUE($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *";
+  const value = [title, 
+    slug, 
+    heroImageUrl, 
+    blogEntryImage, 
+    author,
+    publishdate, 
+    blogShortDescription, 
+    blogContentRich];
+client
+  .query(text, value)
+  .then((data) => res.json(data.rows))
+  .catch((err) => console.log(err))
+})
+
+blogPostRouter.delete("/:slug", (req, res) => {
+  const { slug } = req.params;
+  client 
+    .query("DELETE * FROM blog WHERE slug=$1", [slug])
+    .then((data) => data.json(data.rows))
+    .catch((err) => console.log(err))
+
+})
 
 module.exports = blogPostRouter;
